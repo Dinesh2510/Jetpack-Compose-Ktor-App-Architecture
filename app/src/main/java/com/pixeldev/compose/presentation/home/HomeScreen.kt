0a8e4e3f8692aac28f37e06571d2e6797ef34cc5
Dinesh2510 <dineshchavan7708@outlook.com>
2025-07-09 23:18:30 +0530

    Ui changes

--- app/src/main/java/com/pixeldev/compose/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/pixeldev/compose/presentation/home/HomeScreen.kt
@@ -12,8 +12,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -38,65 +41,82 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.style.TextOverflow
+import com.pixeldev.compose.utils.CenteredLoader
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val state by viewModel.homeResponse.collectAsState()
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFF2F2F2))
-            .padding(16.dp)
-    ) {
-        when (state) {
-            is Resource.Loading -> {
-                CircularProgressIndicator(
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Quotes(Hilt,Retrofit,MVVM)",
+                        style = MaterialTheme.typography.titleLarge,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            )
+        },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFFF2F2F2))
+                    .padding(innerPadding)
+                    .padding(16.dp)
+            ) {
+                when (state) {
+                    is Resource.Loading -> {
+                        CenteredLoader()
+                    }
 
-            is Resource.Success -> {
-                val quotes = (state as Resource.Success).data?.quotes ?: emptyList()
-                if (quotes.isNotEmpty()) {
+                    is Resource.Success -> {
+                        val quotes = (state as Resource.Success).data?.quotes ?: emptyList()
+                        if (quotes.isNotEmpty()) {
 
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(Color(0xFFF2F2F2))
-                            .padding(16.dp)
-                    ) {
-                        items(quotes.size) { quote ->
-                            QuoteCard(quotes[quote])
-                            Spacer(modifier = Modifier.height(12.dp))
+                            LazyColumn(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(Color(0xFFF2F2F2))
+                                    .padding(16.dp)
+                            ) {
+                                items(quotes.size) { quote ->
+                                    QuoteCard(quotes[quote])
+                                    Spacer(modifier = Modifier.height(12.dp))
+                                }
+                            }
+                        } else {
+                            Text(
+                                text = "No quotes available.",
+                                modifier = Modifier.align(Alignment.Center),
+                                style = MaterialTheme.typography.bodyLarge
+                            )
                         }
                     }
-                } else {
-                    Text(
-                        text = "No quotes available.",
-                        modifier = Modifier.align(Alignment.Center),
-                        style = MaterialTheme.typography.bodyLarge
-                    )
+
+                    is Resource.Error -> {
+                        Text(
+                            text = "Failed to load quotes: ${(state as Resource.Error).message ?: "Unknown error"}",
+                            color = Color.Red,
+                            modifier = Modifier.align(Alignment.Center),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+
+                    null -> {
+                        // Initial state - optionally show nothing or loading
+                        CenteredLoader()
+                    }
                 }
             }
 
-            is Resource.Error -> {
-                Text(
-                    text = "Failed to load quotes: ${(state as Resource.Error).message ?: "Unknown error"}",
-                    color = Color.Red,
-                    modifier = Modifier.align(Alignment.Center),
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            }
-
-            null -> {
-                // Initial state - optionally show nothing or loading
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-            }
-        }
-    }
-
-
+        })
 }
 
 
